29b3b2293c01c86f3a8f70db319a14a2d73129aa
mayunhai <414199639@qq.com>
2020-01-20 14:24:38 +0800

    update

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,9 @@
+export default () => {
+  window._hmt = window._hmt || [];
+  (function() {
+    var hm = document.createElement('script');
+    hm.src = 'https://hm.baidu.com/hm.js?ede2ba09d1e5c979d7ffc0075bd809e9';
+    var s = document.getElementsByTagName('script')[0]; 
+    s.parentNode.insertBefore(hm, s);
+  })();
+}
